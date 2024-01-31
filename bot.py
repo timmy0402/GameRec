48fcec746a2206037d0994f9ab357be43b52be47
timmy0402 <145733724+timmy0402@users.noreply.github.com>
2024-01-31 14:32:43 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,60 +2,39 @@ import requests
 import json
 from flask import request, Flask
 
-def looking_game():
-    url = "https://api.rawg.io/api/genres?key=10c5b7e4c1de4ddb83fb19e736dd91d0"
-    headers = {
-    'x-rapidapi-key': "e6ef05302emsh9c46dd59d7ac5d1p13a2d8jsneb37aae7d452",
-    'x-rapidapi-host': "rawg-video-games-database.p.rapidapi.com"
-    }
-    response = requests.request("GET", url, headers=headers)
-    formatted_response = json.loads((response.text))
-    formatted_response = formatted_response['results']
-    print(formatted_response)
-    return formatted_response
-looking_game()
-app = Flask(__name__)
-@app.route('/',methods=['GET', 'POST'])
-def game_search():
-    result = looking_game()
-    print("hello world")
-    input_information = request.json
-    games_list = [{'name':'g1'},{'name':'g2'},{'name':"g3"}]
-    
-    if input_information['game'] == 'action':
-        games = result[0]['games']
-        for i in range(3):
-            games_list[i]['name'] = games[i]['name']
-    elif input_information['game'] == 'adventure':
-        games = result[1]['games']
-        for i in range(3):
-            games_list[i]['name'] = games[i]['name']
-    elif input_information['game'] == 'puzzle':
-        games = result[8]['games']
-        for i in range(3):
-            games_list[i]['name'] = games[i]['name'] 
-    elif input_information['game'] == 'racing':
-        games = result[12]['games']
-        for i in range(3):
-            games_list[i]['name'] = games[i]['name']
-    elif input_information['game'] == 'RPG':
-        games = result[3]['games']
-        for i in range(3):
-            games_list[i]['name'] = games[i]['name'] 
-    elif input_information['game'] == 'racing':
-        games = result[12]['games']
-        for i in range(3):
-            games_list[i]['name'] = games[i]['name']
-    elif input_information['game'] == 'sport':
-        games = result[13]['games']
-        for i in range(3):
-            games_list[i]['name'] = games[i]['name'] 
-    elif input_information['game'] == 'strategry':
-        games = result[4]['games']
-        for i in range(3):
-            games_list[i]['name'] = games[i]['name'] 
-    
-    return {"response_message":games_list}
+# Constants for API key and URL
+API_KEY = "e6ef05302emsh9c46dd59d7ac5d1p13a2d8jsneb37aae7d452"
+API_URL = "https://api.rawg.io/api/genres?key=10c5b7e4c1de4ddb83fb19e736dd91d0"
 
-if __name__ == "__main__":
-    app.run()
+# Mapping of game genres to their corresponding index in the results
+GENRE_MAPPING = {
+    'action': 0,
+    'adventure': 1,
+    'puzzle': 8,
+    'racing': 12,
+    'RPG': 3,
+    'sport': 13,
+    'strategy': 4,
+}
+
+app = Flask(__name__)
+
+def looking_game():
+    # Set headers for the API request
+    headers = {
+        'x-rapidapi-key': API_KEY,
+        'x-rapidapi-host': "rawg-video-games-database.p.rapidapi.com"
+    }
+    # Make API request to get game genres
+    response = requests.get(API_URL, headers=headers)
+    formatted_response = json.loads(response.text)
+    return formatted_response['results']
+
+@app.route('/', methods=['GET', 'POST'])
+def game_search():
+    # Retrieve game genres information
+    result = looking_game()
+    # Get JSON input from the request
+    input_information = request.json
+    # Initialize the games list with placeholder names
+    games_list = [{'name'
